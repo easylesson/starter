a712940fa0fd88a6858260f048f22d4abe7b63ef
Anayet775 <anayetullah7755@gmail.com>
2020-07-23 23:59:22 +0600

    change a little

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
         document.getElementById("score-" + activePlayer).textContent =
         scores[activePlayer];
         //Check if player won the game
-        if (scores[activePlayer] >= 10) {
+        if (scores[activePlayer] >= 100) {
         document.querySelector("#name-" + activePlayer).textContent = "Winner!";
         diceDom.style.display = "none";
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
